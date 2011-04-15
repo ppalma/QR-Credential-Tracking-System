a9f4b47ef0311bb1fc47e885b1b1e483e393f292
Patricio Palma <mail@ppalma.cl>
2011-04-15 13:50:17 -0400

    fixing some email stuff

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -6,6 +6,7 @@ class UsersController extends AppController {
 
 	var $name = 'Users';
 	var $components = array('Auth','Email');
+
 	function login(){ // Check for incoming login request.
 		if ($this->data) {
 			// Use the AuthComponent’s login action
@@ -34,14 +35,15 @@ class UsersController extends AppController {
 
 		// Allow users to access the following action when not logged in
 		function beforeFilter() {
-			$this->Auth->allow('register', 'thanks', 'confirm', 'logout');
+			//$this->Auth->allow('register', 'thanks', 'confirm', 'logout');
+			$this->Auth->allow('*');
 			$this->Auth->autoRedirect = false;
 		}
 		// Allows a user to sign up for a new account
 		function register() {
 			if (!empty($this->data)) {
 				// See my previous post if this is forgien to you
-				debug($this->data);
+			
 				$this->data['User']['password'] =
 				$this->Auth->password($this->data['User']['password']);
 				$this->User->data = Sanitize::clean($this->data);
@@ -54,7 +56,7 @@ class UsersController extends AppController {
 				}
 				// Failed, clear password field
 				else {
-					$this->data['User']['passwrd'] = null;
+					$this->data['User']['password'] = null;
 				}
 			}
 		}
@@ -65,23 +67,33 @@ class UsersController extends AppController {
 		 */
 		function __sendActivationEmail($user_id) {
 			$user = $this->User->find(
-			array('User.id' => $user_id), array('User.email', 'User.username'), null, false);
+			array('User.id' => $user_id), array('User.id','User.email_address', 'User.username','User.created'), null, false);
+
 			if ($user === false) {
 				debug(__METHOD__." failed to retrieve User data for user.id: {$user_id}");
 				return false;
 			}
-
+			
 			// Set data for the "view" of the Email
+			
+			
 			$this->set('activate_url', 'http://' . env('SERVER_NAME') . '/users/activate/' .
-			$user['User']['id'] . '/' . $this->User->getActivationHash());
+			$user['User']['id']. '/' . 
+			$this->getActivationHash($user));
+			
+			
+		//	debug($this->getActivationHash($user));
+		
 			$this->set('username', $this->data['User']['username']);
 
-			$this->Email->to = $user['User']['email'];
+			$this->Email->to = $user['User']['email_address'];
 			$this->Email->subject = env('SERVER_NAME') . ' – Please confirm your email address';
-			$this->Email->from = 'noreply@' . env('SERVER_NAME');
+			$this->Email->from = 'noreply@ic.uach.cl';
 			$this->Email->template = 'user_confirm';
 			$this->Email->sendAs = 'text';   // you probably want to use both :)
+		//	debug($this->Email);
 			return $this->Email->send();
+			
 		}
 		/**
 		 * Activates a user account from an incoming link
@@ -103,69 +115,83 @@ class UsersController extends AppController {
 
 			// Activation failed, render ‘/views/user/activate.ctp’ which should tell the user.
 		}
-function thanks() {
-	$this->Session->setFlash(_('un wereber'));
-	
-$this->redirect(array('action' => 'index'));
-	}
-function index() {
-	$this->User->recursive = 0;
-	$this->set('users', $this->paginate());
-}
+		
+		function getActivationHash($user = null)
+        {
+                if (!isset($user)) {       
+                        return false;
+                }
+              
+                return substr(
+                Security::hash(Configure::read('Security.salt'). 
+                $user['User']['created']. 
+                date('Ymd')), 
+                0, 
+                8);
+        }
+		function thanks() {
+			$this->Session->setFlash(_('un wereber'));
 
-function view($id = null) {
-	if (!$id) {
-		$this->Session->setFlash(__('Invalid user', true));
-		$this->redirect(array('action' => 'index'));
-	}
-	$this->set('user', $this->User->read(null, $id));
-}
-
-function add() {
-	if (!empty($this->data)) {
-		$this->User->create();
-		if ($this->User->save($this->data)) {
-			$this->Session->setFlash(__('The user has been saved', true));
 			$this->redirect(array('action' => 'index'));
-		} else {
-			$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
 		}
-	}
-	$groups = $this->User->Group->find('list');
-	$this->set(compact('groups'));
-}
+		function index() {
+			$this->User->recursive = 0;
+			$this->set('users', $this->paginate());
+		}
 
-function edit($id = null) {
-	if (!$id && empty($this->data)) {
-		$this->Session->setFlash(__('Invalid user', true));
-		$this->redirect(array('action' => 'index'));
-	}
-	if (!empty($this->data)) {
-		if ($this->User->save($this->data)) {
-			$this->Session->setFlash(__('The user has been saved', true));
+		function view($id = null) {
+			if (!$id) {
+				$this->Session->setFlash(__('Invalid user', true));
+				$this->redirect(array('action' => 'index'));
+			}
+			$this->set('user', $this->User->read(null, $id));
+		}
+
+		function add() {
+			if (!empty($this->data)) {
+				$this->User->create();
+				if ($this->User->save($this->data)) {
+					$this->Session->setFlash(__('The user has been saved', true));
+					$this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+				}
+			}
+			$groups = $this->User->Group->find('list');
+			$this->set(compact('groups'));
+		}
+
+		function edit($id = null) {
+			if (!$id && empty($this->data)) {
+				$this->Session->setFlash(__('Invalid user', true));
+				$this->redirect(array('action' => 'index'));
+			}
+			if (!empty($this->data)) {
+				if ($this->User->save($this->data)) {
+					$this->Session->setFlash(__('The user has been saved', true));
+					$this->redirect(array('action' => 'index'));
+				} else {
+					$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
+				}
+			}
+			if (empty($this->data)) {
+				$this->data = $this->User->read(null, $id);
+			}
+			$groups = $this->User->Group->find('list');
+			$this->set(compact('groups'));
+		}
+
+		function delete($id = null) {
+			if (!$id) {
+				$this->Session->setFlash(__('Invalid id for user', true));
+				$this->redirect(array('action'=>'index'));
+			}
+			if ($this->User->delete($id)) {
+				$this->Session->setFlash(__('User deleted', true));
+				$this->redirect(array('action'=>'index'));
+			}
+			$this->Session->setFlash(__('User was not deleted', true));
 			$this->redirect(array('action' => 'index'));
-		} else {
-			$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
 		}
-	}
-	if (empty($this->data)) {
-		$this->data = $this->User->read(null, $id);
-	}
-	$groups = $this->User->Group->find('list');
-	$this->set(compact('groups'));
-}
-
-function delete($id = null) {
-	if (!$id) {
-		$this->Session->setFlash(__('Invalid id for user', true));
-		$this->redirect(array('action'=>'index'));
-	}
-	if ($this->User->delete($id)) {
-		$this->Session->setFlash(__('User deleted', true));
-		$this->redirect(array('action'=>'index'));
-	}
-	$this->Session->setFlash(__('User was not deleted', true));
-	$this->redirect(array('action' => 'index'));
-}
 }
 ?>
